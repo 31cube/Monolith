5743d40b5e87675a48878b776bb2db3dc7329acb
31cube <zxenozxeno28@gmail.com>
2025-09-20 18:21:42 +0100

    Update Vent.cs
    Fixed variable names.

--- Patch/Vent.cs
+++ Patch/Vent.cs
@@ -11,18 +11,18 @@ using Monolith;
 [HarmonyPatch(typeof(Vent), nameof(Vent.CanUse))]
 public static class PVent
 {
-    public static void Postfix(Vent __instance, NetworkedPlayerInfo Pc, ref bool CanUse, ref bool CouldUse, ref float Result)
+    public static void Postfix(Vent __instance, NetworkedPlayerInfo pc, ref bool canUse, ref bool couldUse, ref float __result)
     {
         if (Toggle.UseVents == true)
         {
             float Number = float.MaxValue;
-            PlayerControl PlayerObj = Pc.Object;
+            PlayerControl PlayerObj = pc.Object;
             Vector3 Center = PlayerObj.Collider.bounds.center;
             Vector3 Position = __instance.transform.position;
             Number = Vector3.Distance(Center, Position);
-            CanUse = Number <= __instance.UsableDistance && !PhysicsHelpers.AnythingBetween(PlayerObj.Collider, Center, Position, Constants.ShipOnlyMask, false);
-            CouldUse = true;
-            Result = Number;
+            canUse = Number <= __instance.UsableDistance && !PhysicsHelpers.AnythingBetween(PlayerObj.Collider, Center, Position, Constants.ShipOnlyMask, false);
+            couldUse = true;
+            __result = Number;
         };
     }
 };
