79176a258c4dafa7987e817c7fdec80367672911
31cube <zxenozxeno28@gmail.com>
2025-09-20 23:23:20 +0100

    Update Cheat.cs

--- Utilities/Cheat.cs
+++ Utilities/Cheat.cs
@@ -1,1 +1,33 @@
+using AmongUs.GameOptions;
+using Il2CppInterop.Runtime;
+using Il2CppSystem;
+using Monolith;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
 
+/*
+namespace Monolith
+{
+    public class Cheat
+    {
+        public static void ReportBody()
+        {
+            if (!Util.PPMActive)
+            {
+                var PlayerList = new List<NetworkedPlayerInfo>();
+                foreach (var player in PlayerControl.AllPlayerControls)
+                {
+                    PlayerList.Add(player.Data);
+                };
+                Util.PlayerPickerMenu(PlayerList, (System.Action)(() =>
+                {
+                    Util.ReportBody(Util.PPMTarget);
+                    Util.PPMActive = false;
+                }));
+            };
+        }
+    };
+}; 
+*/
