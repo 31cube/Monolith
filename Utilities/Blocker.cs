0914fceca366aec3c5a0fb9d2756d84d9c2af838
31cube <zxenozxeno28@gmail.com>
2025-09-20 19:57:16 +0100

    Update Blocker.cs

--- Utilities/Blocker.cs
+++ Utilities/Blocker.cs
@@ -10,13 +10,6 @@ public class Blocker : MonoBehaviour
 {
     public void Load()
     {
-        Analytics.enabled = false;
-        Analytics.deviceStatsEnabled = false;
-        PerformanceReporting.enabled = false;
-        /* 
-         * AS OF 20TH OF SEP 2025 THERE IS NO AMONG US ANTICHEAT,
-         * I DISABLED UNITY ANALYTICS ABOVE,
-         * ONLY REAL DETECTIONS ARE ALL SERVERSIDE.
-        */
+        
     }
 };
