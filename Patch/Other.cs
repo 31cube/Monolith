e369c877eb66d8654b719cfb25473f045561be7c
31cube <zxenozxeno28@gmail.com>
2025-09-20 18:01:44 +0100

    Update Other.cs

--- Patch/Other.cs
+++ Patch/Other.cs
@@ -1,4 +1,4 @@
-﻿using AmongUs.Data;
+using AmongUs.Data;
 using BepInEx.Logging;
 using HarmonyLib;
 using Monolith;
@@ -10,45 +10,45 @@ using System.Security.Cryptography;
 [HarmonyPatch(typeof(HatManager), nameof(HatManager.Initialize))]
 public static class PHatManager
 {
-    public static void Postfix(HatManager Instance)
+    public static void Postfix(HatManager __instance)
     {
-        foreach (var Bundle in Instance.allBundles)
+        foreach (var Bundle in __instance.allBundles)
         {
             Bundle.Free = true;
         };
-        foreach (var AllBundle in Instance.allFeaturedBundles)
+        foreach (var AllBundle in __instance.allFeaturedBundles)
         {
             AllBundle.Free = true;
         };
-        foreach (var Cube in Instance.allFeaturedCubes)
+        foreach (var Cube in __instance.allFeaturedCubes)
         {
             Cube.Free = true;
         };
-        foreach (var Item in Instance.allFeaturedItems)
+        foreach (var Item in __instance.allFeaturedItems)
         {
             Item.Free = true;
         };
-        foreach (var Hat in Instance.allHats)
+        foreach (var Hat in __instance.allHats)
         {
             Hat.Free = true;
         };
-        foreach (var NamePlate in Instance.allNamePlates)
+        foreach (var NamePlate in __instance.allNamePlates)
         {
             NamePlate.Free = true;
         };
-        foreach (var Pet in Instance.allPets)
+        foreach (var Pet in __instance.allPets)
         {
             Pet.Free = true;
         };
-        foreach (var Skin in Instance.allSkins)
+        foreach (var Skin in __instance.allSkins)
         {
             Skin.Free = true;
         };
-        foreach (var StarBundle in Instance.allStarBundles)
+        foreach (var StarBundle in __instance.allStarBundles)
         {
             StarBundle.price = 0;
         };
-        foreach (var Visor in Instance.allVisors)
+        foreach (var Visor in __instance.allVisors)
         {
             Visor.Free = true;
         };
@@ -58,10 +58,10 @@ public static class PHatManager
 [HarmonyPatch(typeof(VersionShower), nameof(VersionShower.Start))]
 public static class PVersionShower
 {
-    public static void Postfix(VersionShower Instance)
+    public static void Postfix(VersionShower __instance)
     {
-        if (Instance == null) return;
-        Instance.text.text = $"Monolith ~ {Application.version}";
+        if (__instance == null) return;
+        __instance.text.text = $"Monolith ~ {Application.version}";
     }
 };
 
@@ -116,9 +116,9 @@ public static class PAmongUsClient
 [HarmonyPatch(typeof(PlatformSpecificData), nameof(PlatformSpecificData.Serialize))]
 public static class PPlatformSpecificData
 {
-    public static void Postfix(PlatformSpecificData Instance)
+    public static void Postfix(PlatformSpecificData __instance)
     {
         int Num = new int[] { 10, 9, 8 }[UnityEngine.Random.Range(0, 3)];
-        Instance.Platform = (Platforms)Num;
+        __instance.Platform = (Platforms)Num;
     }
 };
